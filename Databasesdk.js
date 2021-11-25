e5ed0a1c649b624e8b0bd4d3d573e5515bd72c13
SurajMali <76955549+SurajMali002@users.noreply.github.com>
2021-11-25 16:56:17 +0530

    Update Databasesdk.js

--- Databasesdk.js
+++ Databasesdk.js
@@ -1,13 +1,15 @@
 
 // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyC-DdgHwBUNJT46Fhq0DpkBuk4cHxpYUKI",
-    authDomain: "sociallyoauth.firebaseapp.com",
-    projectId: "sociallyoauth",
-    storageBucket: "sociallyoauth.appspot.com",
-    messagingSenderId: "597979831746",
-    appId: "1:597979831746:web:7d2ada39a3dbe684e80a5b"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyBZlX3T9ryR6oQDRJtkpteYS5bVJpp88z0",
+  authDomain: "socially-1df04.firebaseapp.com",
+  databaseURL: "https://socially-1df04-default-rtdb.firebaseio.com",
+  projectId: "socially-1df04",
+  storageBucket: "socially-1df04.appspot.com",
+  messagingSenderId: "408212621806",
+  appId: "1:408212621806:web:5ae3d315e144c86a5766ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   var db = firebase.firestore();
